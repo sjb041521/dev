2620734ad434d15aec8cf0afc5ba3254c76dd171
ParkMyeongJin <77596466+sjb041521@users.noreply.github.com>
2021-09-16 12:43:15 +0900

    Add files via upload

---
+++ frontend/src/component/footer.js
@@ -0,0 +1,12 @@
+import React, { Component, } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar,NavDropdown,Nav,Container} from "react-bootstrap";
+class Footer extends Component { 
+    render() { 
+    return ( 
+        <footer className="footer">Made By PMJ</footer>
+     );
+    }
+    }
+     export default Footer;
+
